73567287b991168d2e20a0033d137237e2f5a5d9
brunningman <abrunner221@gmail.com>
2019-09-13 07:44:38 -0700

    send SMS to all subs function

---
+++ server/actions/sendSMStoAllSubs.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const sendSMS =  require('./sendSMS');
+
+const sendSMStoAllSubs = (body, url, subs, callback) => {
+  subs.forEaca(sub => {
+    const message = {
+      body: body,
+      to: sub.phone,
+      from: process.env.TWILIO_PHONE
+    }
+
+    if(url) {
+      message.mediaURL = url;
+    }
+    sendSMS(message);
+  })
+}
+
+module.exports = sendSMStoAllSubs;
